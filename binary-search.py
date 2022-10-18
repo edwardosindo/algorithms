6768e12b0f3391e73a002ed93eb44cf96f530c23
Edward Osindo <eddu.almasi@gmail.com>
2022-10-18 12:16:50 +0300

    Added comments to the recursive function for how i implemented the solution

--- binary-search.py
+++ binary-search.py
@@ -11,16 +11,6 @@ def linear_search(data, target):
     return False
 
 # Iterative Binary Search
-
-# Step-by-step Binary Search Algorithm: We basically ignore half of the elements just after one comparison.
-
-# 1. Compare x with the middle element.
-# 2. If x matches with the middle element, we return the mid index.
-# 3. Else If x is greater than the mid element, then x can only lie in the right half subarray after the mid element.
-# So we recur for the right half.
-# 4. Else (x is smaller) recur for the left half.
-
-
 def binary_search_iterative(data, target):
     low = 0
     high = len(data) - 1
@@ -37,6 +27,15 @@ def binary_search_iterative(data, target):
 
 
 # # Recursive Binary Search
+
+# Step-by-step Binary Search Algorithm: We basically ignore half of the elements just after one comparison.
+
+# 1. Compare x with the middle element.
+# 2. If x matches with the middle element, we return the mid index.
+# 3. Else If x is greater than the mid element, then x can only lie in the right half subarray after the mid element.
+# So we recur for the right half.
+# 4. Else (x is smaller) recur for the left half.
+
 def binary_search_recursive(data,target, low, high):
     # Base function
     if low > high:
